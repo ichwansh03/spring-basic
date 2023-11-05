bc9f5274b918f2c9ee0a672512c7e6132d3c349b
Ichwan <ichwansholihin03@gmail.com>
2023-11-05 10:09:28 +0700

    chore: add bean configuration

--- src/main/java/com/ichwan/basic/BeanConfiguration.java
+++ src/main/java/com/ichwan/basic/BeanConfiguration.java
@@ -4,15 +4,24 @@ import com.ichwan.basic.data.Foo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Slf4j
 @Configuration
 public class BeanConfiguration {
 
-    @Bean
-    public Foo foo(){
+    @Primary
+    @Bean(value = "fooFirst")
+    public Foo foo1(){
         Foo foo = new Foo();
-        log.info("created new foo");
+        log.info("created new foo1");
+        return foo;
+    }
+
+    @Bean(value = "fooSecond")
+    public Foo foo2(){
+        Foo foo = new Foo();
+        log.info("created new foo2");
         return foo;
     }
 }
--- src/test/java/com/ichwan/basic/BeanTest.java
+++ src/test/java/com/ichwan/basic/BeanTest.java
@@ -2,28 +2,58 @@ package com.ichwan.basic;
 
 import com.ichwan.basic.data.Foo;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class BeanTest {
 
+    private ApplicationContext context;
+
+    @BeforeEach
+    void setUp() {
+        context = new AnnotationConfigApplicationContext(BeanConfiguration.class);
+    }
+
     @Test
     void beanTest() {
-        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfiguration.class);
 
         //walaupun bean blm diinisialisasi, dia akan terlebih dahulu dipanggil diawal.
         Assertions.assertNotNull(context);
     }
 
+    //jika object bean hanya 1/singleton
     @Test
     void getBeanTest() {
-        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfiguration.class);
 
         Foo foo1 = context.getBean(Foo.class);
         Foo foo2 = context.getBean(Foo.class);
 
         //object bean tetap dibuat sekali
         Assertions.assertSame(foo1, foo2);
+    }
+
+    //jika terdapat object bean dgn tipe yg sama, inject nama methodnya
+    @Test
+    void getDuplicateBeanTest(){
+
+        Foo foo1 = context.getBean("fooFirst",Foo.class);
+        Foo foo2 = context.getBean("fooSecond",Foo.class);
+
+        //object bean tetap dibuat sekali
+        Assertions.assertNotSame(foo1, foo2);
+    }
+
+    @Test
+    void getPrimaryBean() {
+        Foo foo = context.getBean(Foo.class);
+
+        Foo foo1 = context.getBean("fooFirst",Foo.class);
+        Foo foo2 = context.getBean("fooSecond",Foo.class);
+
+        Assertions.assertSame(foo, foo1);
+
+        Assertions.assertNotSame(foo, foo2);
     }
 }
